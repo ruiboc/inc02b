5f28b2286118edf3ff0b60551767700a2b5b6379
ruiboc <ruiboc@uw.edu>
2021-04-10 03:16:41 +0800

    add a script that computes lightyear

---
+++ inc02b.R
@@ -0,0 +1,8 @@
+c <- 30000
+lightMinute <- c*60
+lightHour <- lightMinute*60
+lightDay <- lightHour*24
+lightYear <- lightDay*365
+cat("length of light year:", lightYear,"\n",sep=" ")
+cat("That's too long")
+
